cce3b25731c0807c60e85140a92a6e79461058eb
Jon Christensen <jon.christensen@10up.com>
2019-05-01 13:12:31 -0500

    test es6

--- assets/js/frontend/frontend.js
+++ assets/js/frontend/frontend.js
@@ -1,1 +1,12 @@
 // import foo from './components/bar';
+
+const bar = 'foo';
+
+/**
+ * Hello
+ */
+const hello = ( localBar = 'localBar' ) => {
+	console.log( 'world', localBar );
+};
+
+hello( bar );
